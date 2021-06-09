5d8e310720e76a00014884ef0183f24d8c19f059
Lewin Probst <info@emirror.de>
2021-06-09 15:59:49 +0200

    Added tailwind.config.js.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+      './public_html/**/*.html',
+      './scss/**/*.scss'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
